6a2e65741299e827010c884860a86ea359646b71
Aarju Shaikh <aarjushaikh02@gmail.com>
2023-03-08 23:22:01 +0530

    first commit

---
+++ exmpbyshelly/welcome.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Fruits</title>
+</head>
+<body>
+    <h1> Below are the type of Meals </h1>
+    
+      &#8226; Fruit
+      &#x2022; Bananas
+      <li> Apples</li>
+        
+</body>
+</html>
